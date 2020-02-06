73b3c0213efccb4d8026eb87d678da6984d15736
PrinceDevadoss <prince.devadoss@freshworks.com>
2020-02-06 15:58:09 +0530

    Repeater Program Changes

--- problems/problem1.c
+++ problems/problem1.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int next(int *src, int len, int count, int pred, int checker, int m) {
-    if (m<len-1) {
-        if((pred == 0 && *(src)+1 == checker) || (pred == 2 &&*(src)+1 == checker) || checker == *(src)) {
+    if (m<len) {
+        if (checker == *(src)) {
+            return next(src+1, len, count+1, pred, checker, m+1);
+        }
+        else if((pred == 0 && *(src)+1 == checker) || (pred == 2 &&*(src)+1 == checker)) {
             pred = 1;
             checker = *(src);
             return next(src+1, len, count+1, pred, checker, m+1);
         }
-        else if((pred == 0 && *(src)-1 == checker) || (pred == 1 && *(src)-1 == checker) || checker == *(src)) {
+        else if((pred == 0 && *(src)-1 == checker) || (pred == 1 && *(src)-1 == checker)) {
             pred = 2;
             checker = *(src);
             return next(src+1, len, count+1, pred, checker, m+1);
@@ -26,7 +29,6 @@ int router(int *src, int nexter, int length, int *arr, int count) {
     if(nexter<length) {
         result = next(src+nexter, length, 0, 0, *src, nexter);
         if(result > 0) {
-            printf("%d\t", result);
             *arr = result;
             return router(src, nexter+1, length, arr+1, count+1);
         }
@@ -43,9 +45,10 @@ int finder(int *src, int start, int length, int *arr, int count) {
     int res;
     if(start<length) {
         res = router(src, 1, length, arr, count);
+        if (count != res) {
+            arr += (res - count);
+        }
         count = res;
-        arr += res;
-        printf("res ---- > %d, %d\n", res, count);
         return finder(src+1, start+1, length, arr, count);
     }
     else {
@@ -69,6 +72,6 @@ int main() {
     for (int j=0;j<result; j++) {
         printf("%d,", *(arr+j));
     }
-    printf("\nResult is %d\n", result);
+    printf("\nResult is %d\n", max+1);
     return 0;
 }
