d53d63eec6b4b7e2ca9eda4d5d8ef6a8c0d61e29
yasicarajesh <yasicarajesh@gmail.com>
2020-02-07 07:38:20 -0800

    adding the hub c program

---
+++ problems/hub.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int ger(int p[]2, int len) {
+if(len > 0) {
+return p[len] + ger(p, len-1) + ger(p, len-2);
+}
+else {
+return p[len];
+}
+}
+
+int main() {
+int h[]={1,2,3,4,5};
+printf("%d\n", ger(h,4));
+return 0;
+}
